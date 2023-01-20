15996e0ec067ca8be26912a0754d782b68d1e5b8
Thomas Szafir <thomasszafir@Thomass-MacBook-Air.local>
2023-01-19 22:32:05 -0600

    ur momma

--- app/src/pages/index.js
+++ app/src/pages/index.js
@@ -5,6 +5,8 @@ import Roll from 'react-reveal/Roll';
 
 import logo from '../images/logo.png';
 import communitySVG from '../images/community.svg';
+import com2 from '../images/com2.png';
+import com3 from '../images/com3.png';
 
 import '../global/global.scss';
 import './index.scss';
@@ -39,7 +41,7 @@ const LandingComponent = () => {
           <div className="title"><Fade up>Green Keys @ UChicago</Fade></div>
           <div className="subtitle"><Fade delay={400} up>Creating Green Keys Finance at UChicago</Fade></div>
           <Fade delay={800} up><div className="buttonParent">
-            <a href="https://zubatomic.com/metaverse/redirect" target="_blank">
+            <a href="https://forms.gle/tboYkc6ZU6akRxoY8" target="_blank">
               <Blob>
               <div className="button">Join!</div>
               </Blob>
@@ -76,12 +78,12 @@ const AfterMain = () => {
             <div className="description">We are a community-first organization. Our principal aim is to provide true tight-knit communities to students at UChicago from all walks of life. The creation of Green Keys thus becomes just a tool to achieve this. </div>
           </div></Fade>
           <Fade delay={400} up><div className="box">
-            <div className="icon"><img src={communitySVG}></img></div>
+            <div className="icon"><img src={com2}></img></div>
             <div className="heading">Exploration</div>
             <div className="description">Innovation, exploration and creation is at the root of our culture. It is through relentless iteration that we aim to tackle complex phenomena such as capital markets. New ideas are always welcomed.</div>
           </div></Fade>
           <Fade delay={800} up><div className="box">
-            <div className="icon"><img src={communitySVG}></img></div>
+            <div className="icon"><img src={com3}></img></div>
             <div className="heading">Growth</div>
             <div className="description">We strive to accompany every UChicago student through the exciting, yet sometimes rocky experience that is entering University for the first time by providing a safe-space for personal growth and community.</div>
           </div></Fade>
@@ -107,7 +109,7 @@ const JoinSlide = () => {
       <div className="js" id="join">
         {currentSemester != "Summer" && 
           <div className="option">
-            <Fade right><a href="https://zubatomic.com/metaverse/redirect" target="_blank"><div className="link">Go to main site</div></a></Fade>
+            <Fade right><a href="https://forms.gle/tboYkc6ZU6akRxoY8" target="_blank"><div className="link">Apply</div></a></Fade>
             <div className="header">INTERESTED IN JOINING?</div>
             <div className="subheader">Open to undergraduate students this <b>{currentSemester} {currentYear}</b>.</div>
             <div className="text">Membership is open for {currentSemester.toLowerCase()}! Find a community at UChicago, and have fun while doing so, by joining Green Keys@Uchicago. Membership is a right of every UChicago student and thus joining is as simple as signing up. We will be holding our kickoff meeting soon, so be sure to sign up soon!</div>
@@ -115,17 +117,17 @@ const JoinSlide = () => {
         }
         {currentSemester == "Summer" && 
           <div className="option">
-            <Fade right><a href="https://zubatomic.com/metaverse/redirect" target="_blank"><div className="link">Go to main site</div></a></Fade>
+            <Fade right><a href="https://forms.gle/tboYkc6ZU6akRxoY8" target="_blank"><div className="link">Apply</div></a></Fade>
             <div className="header">INTERESTED IN JOINING?</div>
             <div className="subheader">Open to undergraduate students during <b>{currentYear}</b>.</div>
             <div className="text">Membership will open during fall later this year. Find a community at UChicago, and have fun while doing so, by joining Green Keys@UChicago. Membership is a right of every UChicago student and thus joining is as simple as signing up.</div>
           </div>
         }
         <div className="option">
-        <Fade right><a href="https://road-message-514.notion.site/Become-an-officer-1ec0173227154930b79700ad7e3e14f2" target="_blank"><div className="link">Go to officer site</div></a></Fade>
+        <Fade right><a href="https://forms.gle/LdcaXDz628a7aFzE7" target="_blank"><div className="link">Go to officer site</div></a></Fade>
           <div className="header">INTERESTED IN HELPING?</div>
           <div className="subheader">Apply to become an officer through the officer site.</div>
-          <div className="text">We are always looking for extra hands to help us out constructing and running the metaverse. Officers are given limitless creative freedom, and have the opportunity to get hands-on experience running a real-world project. </div>
+          <div className="text">We are always looking for extra hands to help us out constructing and running the club. Officers are given limitless creative freedom, and have the opportunity to get hands-on experience running a real-world project. </div>
         </div>
       </div>
     </Slide>
